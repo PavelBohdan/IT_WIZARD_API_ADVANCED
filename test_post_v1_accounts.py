6ac248acaa7846a823c381b9565165cecbc9b21e
p.bogdan <p.bogdan@vekas-automation.ru>
2025-03-10 15:57:01 +0300

    Add assertions and getting token

--- test_post_v1_accounts.py
+++ test_post_v1_accounts.py
@@ -1,10 +1,11 @@
 from pprint import pprint
+from json import loads
 
 import requests
 
 
 def test_post_v1_account():
-    login: str = 'pavel1'
+    login: str = 'pavel6'
     password: str = '12345678'
     email: str = f'{login}@mail.com'
     json_data = {
@@ -18,6 +19,8 @@ def test_post_v1_account():
     response = requests.post(
         'http://5.63.153.31:5051/v1/account', json=json_data)
     pprint(response.status_code)
+    assert response.status_code == 201, 'Пользователь не был создан'
+
     # Получить письма из почтового сервера
 
     params = {
@@ -26,16 +29,28 @@ def test_post_v1_account():
 
     response = requests.get('http://5.63.153.31:5025/api/v2/messages',
                             params=params, verify=False)
-
     pprint(response.status_code)
+    assert response.status_code == 200, 'Письма не были получены'
     # Получить активационный токен
 
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+
+        if user_login == login:
+            token = user_data['ConfirmationLinkUrl'].split('/').pop()
+            pprint(user_login)
+            pprint(token)
+            break
+    assert token, f'Токен для пользователя: {login} не был получен'
     # Активация пользователя
 
     response = requests.put(
-        'http://5.63.153.31:5051/v1/account/86bb6f4f-b27a-469f-8c52-2a057b180da9')
+        f'http://5.63.153.31:5051/v1/account/{token}')
 
-    pprint(response.status_code)
+    pprint(response.json())
+    assert response.status_code == 200, 'Пользователь не был активирован'
+
     # Авторизация
 
     json_data = {
@@ -47,3 +62,4 @@ def test_post_v1_account():
     response = requests.post(
         'http://5.63.153.31:5051/v1/account/login', json=json_data)
     pprint(response.status_code)
+    assert response.status_code == 200, 'Пользователь не был авторизован'

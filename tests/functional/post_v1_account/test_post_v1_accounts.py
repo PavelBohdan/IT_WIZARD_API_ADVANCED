50b82729f96c578b0e21ccbe5338f731679d30d4
p.bogdan <p.bogdan@vekas-automation.ru>
2025-03-10 17:13:32 +0300

    Написали джобу для запуска тестов

--- tests/functional/post_v1_account/test_post_v1_accounts.py
+++ tests/functional/post_v1_account/test_post_v1_accounts.py
@@ -10,7 +10,7 @@ def test_post_v1_account():
     mailhog_api = MailHogApi(host='http://5.63.153.31:5025')
 
     # Регистрация пользователя
-    login: str = 'pavel10'
+    login: str = 'pavel11'
     password: str = '12345678'
     email: str = f'{login}@mail.com'
     json_data = {
